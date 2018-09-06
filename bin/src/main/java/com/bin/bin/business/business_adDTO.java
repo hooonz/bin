1ab3508fe567e1bcb3f2172247920baee5f655a8
bit-user <bit-user@bit>
2018-09-06 17:10:29 +0900

    2018-09-06 17:09 src/main/java DTO추가

---
+++ bin/src/main/java/com/bin/bin/business/business_adDTO.java
@@ -0,0 +1,26 @@
+package com.bin.bin.business;
+
+public class business_adDTO {
+	private int business_num;
+	private String business_location;
+	private String business_content;
+	private String business_homepage;
+	private String business_phonenumber;
+	private String business_image;
+	private String businessId;
+	
+	public int getBusiness_num() {return business_num;}
+	public void setBusiness_num(int business_num) {this.business_num = business_num;}
+	public String getBusiness_location() {return business_location;}
+	public void setBusiness_location(String business_location) {this.business_location = business_location;}
+	public String getBusiness_content() {return business_content;}
+	public void setBusiness_content(String business_content) {this.business_content = business_content;}
+	public String getBusiness_homepage() {return business_homepage;}
+	public void setBusiness_homepage(String business_homepage) {this.business_homepage = business_homepage;}
+	public String getBusiness_phonenumber() {return business_phonenumber;}
+	public void setBusiness_phonenumber(String business_phonenumber) {this.business_phonenumber = business_phonenumber;}
+	public String getBusiness_image() {return business_image;}
+	public void setBusiness_image(String business_image) {this.business_image = business_image;}
+	public String getBusinessId() {return businessId;}
+	public void setBusinessId(String businessId) {this.businessId = businessId;}
+}
