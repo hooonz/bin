8100f4be768a1f5813d6629b817bb093d6b71729
rlack <rlack@DESKTOP-1D2H2OC>
2018-09-06 17:48:44 +0900

    2018 -09-06

--- bin/src/main/java/com/bin/bin/review/reviewDTO.java
+++ bin/src/main/java/com/bin/bin/review/reviewDTO.java
@@ -21,26 +21,6 @@ public class reviewDTO {
 	public void setUserId(String userId) {this.userId = userId;}
 	public String getBusinessId() {return businessId;}
 	public void setBusinessId(String businessId) {this.businessId = businessId;}
-
-
-	private int review_num;
-	private String review_title;
-	private String review_content;
-	private String review_img;
-	private String userId;
-	private String businessId;
-	
-	public int getReview_num() {return review_num;}
-	public void setReview_num(int review_num) {this.review_num = review_num;}
-	public String getReview_title() {return review_title;}
-	public void setReview_title(String review_title) {this.review_title = review_title;}
-	public String getReview_content() {return review_content;}
-	public void setReview_content(String review_content) {this.review_content = review_content;}
-	public String getReview_img() {return review_img;}
-	public void setReview_img(String review_img) {this.review_img = review_img;}
-	public String getUserId() {return userId;}
-	public void setUserId(String userId) {this.userId = userId;}
-	public String getBusinessId() {return businessId;}
-	public void setBusinessId(String businessId) {this.businessId = businessId;}
-
 }
+
+
